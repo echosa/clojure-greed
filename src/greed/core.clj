37303f7edadd8eff8abe78fd97b141d363d78f20
Brian Zwahr <echosa@gmail.com>
2014-02-15 02:20:02 -0600

    Added board printing and temporary test moves.

--- src/greed/core.clj
+++ src/greed/core.clj
@@ -4,7 +4,25 @@
             [greed.player :refer :all])
   (:gen-class))
 
+(defn print-grid [grid]
+  "Prints the grid to the screen."
+  (mapcat (fn [x]
+            (mapcat (fn [y]
+                      (print (if (= y 0) " " y) " "))
+                    x)
+            (print "\n"))
+          grid)
+  (print "\n"))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Run the game."
   [& args]
-  (println "Hello, World!"))
+  (let [grid [[1 2 3 4 5]
+              [1 2 3 4 5]
+              [1 2 "@" 4 5]
+              [1 2 3 4 5]
+              [1 2 3 4 5]]]
+    (print-grid grid)
+    (print-grid (move-player grid :west))
+    (print-grid (move-player (move-player grid :west) :north))
+    ))
