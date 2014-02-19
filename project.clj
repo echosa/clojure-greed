0137b5eb42a8d0753bfd832e098f4000e18ddb9a
Brian Zwahr <echosa@gmail.com>
2014-02-18 23:41:01 -0600

    Removed currently unused lein-ancient dependency.

--- project.clj
+++ project.clj
@@ -5,8 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clojure-lanterna "0.9.4"]]
-  :plugins [[lein-ancient "0.5.4"]
-            [lein-cloverage "1.0.2"]]
+  :plugins [[lein-cloverage "1.0.2"]]
   :main ^:skip-aot greed.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
