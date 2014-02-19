43de54a60df090ac8f9b23fa7e1851bcfeaa1ada
Brian Zwahr <echosa@gmail.com>
2014-02-18 23:42:17 -0600

    Updated project.clj information.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject greed "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "This is a port of the DOS/BSD freeware game greed."
+  :url "http://github.com/echosa/clojure-greed"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
