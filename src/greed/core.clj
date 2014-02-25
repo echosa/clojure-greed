e24db57a5f0c1cfc04ac149df91ed951e5849639
Brian Zwahr <echosa@gmail.com>
2014-02-24 18:15:39 -0600

    Cleaned up core.clj a bit.

--- src/greed/core.clj
+++ src/greed/core.clj
@@ -7,29 +7,36 @@
 
 (defn player-turn
   "This function is called recursively over and over, serving as the game loop."
+  ([grid direction show-moves]
+     (player-turn (move-player grid direction) show-moves))
+
   ([grid show-moves]
      (print-screen grid show-moves)
      (when-not (= '() (get-player-moves grid))
-       (let [key (wait-for-key)]
-         (condp = key
-           \q (do
-                (print-message "Really quit? (y/n)" grid)
-                (when-not (= (wait-for-key) \y)
-                  (print-message "                  " grid)
-                  (player-turn grid show-moves)))
-           \y (player-turn (move-player grid :northwest) show-moves)
-           \k (player-turn (move-player grid :north) show-moves)
-           \u (player-turn (move-player grid :northeast) show-moves)
-           \l (player-turn (move-player grid :east) show-moves)
-           \n (player-turn (move-player grid :southeast) show-moves)
-           \j (player-turn (move-player grid :south) show-moves)
-           \b (player-turn (move-player grid :southwest) show-moves)
-           \h (player-turn (move-player grid :west) show-moves)
-           \p (player-turn grid (not show-moves))
-           (player-turn grid show-moves)))))
+       (condp = (wait-for-key)
+         \q (do
+              (print-message "Really quit? (y/n)" grid)
+              (when-not (= (wait-for-key) \y)
+                (print-message "                  " grid)
+                (player-turn grid show-moves)))
+         \y (player-turn grid :northwest show-moves)
+         \k (player-turn grid :north show-moves)
+         \u (player-turn grid :northeast show-moves)
+         \l (player-turn grid :east show-moves)
+         \n (player-turn grid :southeast show-moves)
+         \j (player-turn grid :south show-moves)
+         \b (player-turn grid :southwest show-moves)
+         \h (player-turn grid :west show-moves)
+         \p (player-turn grid (not show-moves))
+         (player-turn grid show-moves))))
 
   ([grid]
      (player-turn grid false)))
+
+(defn exit-game
+  [grid]
+  (print-message "Game over. Press q to exit." grid) 
+  (while (not= \q (wait-for-key))))
 
 (defn -main
   "Run the game."
@@ -37,6 +44,5 @@
   (start-display)
   (let [grid (place-character-on-grid (generate-grid 22 79))]
     (player-turn grid)
-    (print-message "Game over. Press q to exit." grid))
-  (while (not= \q (wait-for-key)))
+    (exit-game grid))
   (stop-display))
