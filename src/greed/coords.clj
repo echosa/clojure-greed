786bece98244153ff9e20371d051a4ddf73c11ec
Brian Zwahr <echosa@gmail.com>
2014-03-11 22:27:41 -0500

    Fixed and cleaned up coords and annotations.

--- src/greed/coords.clj
+++ src/greed/coords.clj
@@ -1,86 +1,81 @@
 (ns greed.coords
   (:require [clojure.core.typed :refer :all])
-  (:import [clojure.lang ISeq IPersistentList]))
+  (:import [clojure.lang IPersistentVector]))
 
 (ann get-next-coordinate-northwest
-     [(ISeq (ISeq Any)) (Option Number) (Option Number)
+     [(IPersistentVector (IPersistentVector Any)) Number Number
       ->
       (Option (Vector* Number Number))])
 (defn get-next-coordinate-northwest [grid x y]
-  (when (and x y grid
-             (> x 0) (> y 0))
+  (when (and (> x 0) (> y 0))
     [(- x 1) (- y 1)]))
 
 (ann get-next-coordinate-north
-     [(ISeq (ISeq Any)) (Option Number) (Option Number)
+     [(IPersistentVector (IPersistentVector Any)) Number Number
       ->
       (Option (Vector* Number Number))])
 (defn get-next-coordinate-north [grid x y]
-  (when (and x y grid
-             (> y 0))
+  (when (> y 0)
     [x (- y 1)]))
 
 (ann get-next-coordinate-northeast
-     [(ISeq (ISeq Any)) (Option Number) (Option Number)
+     [(IPersistentVector (IPersistentVector Any)) Number Number
       ->
       (Option (Vector* Number Number))])
 (defn get-next-coordinate-northeast [grid x y]
-  (when (and x y grid
-             (< x (- (count (first grid)) 1))
+  (when (and (< x (- (count (first grid)) 1))
              (> y 0))
     [(+ x 1) (- y 1)]))
 
 (ann get-next-coordinate-east
-     [(ISeq (ISeq Any)) (Option Number) (Option Number)
+     [(IPersistentVector (IPersistentVector Any)) Number Number
       ->
       (Option (Vector* Number Number))])
 (defn get-next-coordinate-east [grid x y]
-  (when (and grid x y
-             (< x (- (count (first grid)) 1)))
+  (when (< x (- (count (first grid)) 1))
     [(+ x 1) y]))
 
 (ann get-next-coordinate-southeast
-     [(ISeq (ISeq Any)) (Option Number) (Option Number)
+     [(IPersistentVector (IPersistentVector Any)) Number Number
       ->
       (Option (Vector* Number Number))])
 (defn get-next-coordinate-southeast [grid x y]
-  (when (and grid x y
-             (< x (- (count (first grid)) 1))
+  (when (and (< x (- (count (first grid)) 1))
              (< y (- (count grid) 1)))
     [(+ x 1) (+ y 1)]))
 
 (ann get-next-coordinate-south
-     [(ISeq (ISeq Any)) (Option Number) (Option Number)
+     [(IPersistentVector (IPersistentVector Any)) Number Number
       ->
       (Option (Vector* Number Number))])
 (defn get-next-coordinate-south [grid x y]
-  (when (and grid x y
-             (< y (- (count grid) 1)))
+  (when (< y (- (count grid) 1))
     [x (+ y 1)]))
 
 (ann get-next-coordinate-southwest
-     [(ISeq (ISeq Any)) (Option Number) (Option Number)
+     [(IPersistentVector (IPersistentVector Any)) Number Number
       ->
       (Option (Vector* Number Number))])
 (defn get-next-coordinate-southwest [grid x y]
-  (when (and grid x y
-             (> x 0)
+  (when (and (> x 0)
              (< y (- (count grid) 1)))
     [(- x 1) (+ y 1)]))
 
 (ann get-next-coordinate-west
-     [(ISeq (ISeq Any)) (Option Number) (Option Number)
+     [(IPersistentVector (IPersistentVector Any)) Number Number
       ->
       (Option (Vector* Number Number))])
 (defn get-next-coordinate-west [grid x y]
-  (when-not (or (nil? x) (nil? y) (<= x 0))
+  (when (> x 0)
     [(- x 1) y]))
 
 (ann get-next-coordinate
-     [(ISeq (ISeq Any)) (IPersistentList Number) Symbol -> (Option (Vector* Number Number))])
+     [(IPersistentVector (IPersistentVector Any)) (Vector* Number Number) Symbol
+      ->
+      (Option (Vector* Number Number))])
 (defn get-next-coordinate [grid current-position direction]
-  (let [x (first current-position)
-        y (second current-position)]
+  (let [x (or (first current-position) -1)
+        y (or (second current-position) -1)]
     (condp = direction
       :northwest (get-next-coordinate-northwest grid x y)
       :north (get-next-coordinate-north grid x y)
